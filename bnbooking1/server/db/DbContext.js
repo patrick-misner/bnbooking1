ea831436c48923cee86294b447d8ca3d0e79fe21
Patrick Misner <misner.patrick@gmail.com>
2022-06-30 16:38:56 -0600

    gucci bandana

--- bnbooking1/server/db/DbContext.js
+++ bnbooking1/server/db/DbContext.js
@@ -1,5 +1,6 @@
 import mongoose from 'mongoose'
 import { AccountSchema } from '../models/Account'
+import { AppointmentsSchema } from "../models/Appointment";
 import { ProviderSchema } from "../models/Provider";
 import { ReviewSchema } from "../models/Review";
 import { ValueSchema } from '../models/Value'
@@ -9,6 +10,8 @@ class DbContext {
   Account = mongoose.model('Account', AccountSchema);
   Providers = mongoose.model('Provider', ProviderSchema);
   Reviews = mongoose.model('Review', ReviewSchema);
+
+  Appointments = mongoose.model('Appointment', AppointmentsSchema)
 }
 
 export const dbContext = new DbContext()
