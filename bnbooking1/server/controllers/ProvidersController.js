fc08d12760a5c5c50a1b16c27338eb8580ac1a80
Patrick Misner <misner.patrick@gmail.com>
2022-06-30 11:31:01 -0600

    delete done

--- bnbooking1/server/controllers/ProvidersController.js
+++ bnbooking1/server/controllers/ProvidersController.js
@@ -7,14 +7,28 @@ export class ProvidersController extends BaseController{
     super('api/providers')
     this.router
       .get ('', this.getAll)
+      .get('/:id', this.getById)
       .use (Auth0Provider.getAuthorizedUserInfo)
       .post ('', this.create)
+      .put('/:id', this.edit)
+      .delete('/:id', this.delete)
   }
+ 
+ 
 
   async getAll (req, res, next){
     try {
       const providers = await providersService.getAll(req.query)
       return res.send(providers)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async getById (req, res, next){
+    try {
+      const provider = await providersService.getById(req.params.id)
+      return res.send(provider)
     } catch (error) {
       next(error)
     }
@@ -28,6 +42,26 @@ export class ProvidersController extends BaseController{
     } catch (error) {
       next(error)
     }
+  }
+
+  async edit(req, res, next) {
+    try {
+      req.body.creatorId = req.userInfo.id
+      const update = await providersService.edit(req.params.id, req.body)
+      return res.send(update)
+    } catch (error) {
+     next(error)
+    }
+
+  }
+
+  async delete(req, res, next) {
+      try {
+        const message = await providersService.delete(req.params.id, req.userInfo.id)
+        res.send(message)
+      } catch (error) {
+        next(error)
+      }
   }
 
 }
