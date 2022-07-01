da9c1429d29a832e2eaf3800121c995bf3c4c5a8
Patrick Misner <misner.patrick@gmail.com>
2022-06-30 21:35:19 -0600

    update

--- bnbooking1/server/models/Appointment.js
+++ bnbooking1/server/models/Appointment.js
@@ -6,7 +6,7 @@ const ObjectId = Schema.Types.ObjectId
 export const AppointmentSchema = new Schema({
   providerId: {type: ObjectId, required: true, ref:'Provider'},
   accountId: { type: ObjectId, required: true, ref:'Account'},
-  date: {type: Date, required: true},
+  date: {type: Date,},
   startTime:{type: Number, required: true},
   duration: {type: Number, default: 1}
 },{timestamps: true, toJSON:{virtuals: true}})
