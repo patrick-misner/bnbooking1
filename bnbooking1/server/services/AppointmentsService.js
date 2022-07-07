cbc2692b9cc3ae0b48482543ca05768c1bc010f2
Patrick Misner <misner.patrick@gmail.com>
2022-07-07 17:00:01 -0600

    sorting appointments

--- bnbooking1/server/services/AppointmentsService.js
+++ bnbooking1/server/services/AppointmentsService.js
@@ -3,7 +3,7 @@ import { BadRequest } from "../utils/Errors"
 
 class AppointmentsService{
   async getAccountAppointments(accountId) {
-   const appointments = await dbContext.Appointments.find({accountId})
+   const appointments = await dbContext.Appointments.find({accountId}).sort({date: 1})
    .populate('provider', 'name')
    return appointments
   }
