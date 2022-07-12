22d469e652f1aa877bbb5f21f4c8b3f58a8aee81
Conner Seely <Seely.Conner@gmail.com>
2022-07-12 10:35:54 -0600

    help

--- bnbooking1/server/controllers/ReviewsController.js
+++ bnbooking1/server/controllers/ReviewsController.js
@@ -3,16 +3,15 @@ import { reviewsService } from "../services/ReviewsService";
 import BaseController from "../utils/BaseController";
 
 
-export class ReviewsController extends BaseController{
-  constructor(){
+export class ReviewsController extends BaseController {
+  constructor() {
     super('api/reviews')
     this.router
-      .use (Auth0Provider.getAuthorizedUserInfo)
+      .use(Auth0Provider.getAuthorizedUserInfo)
       .post('', this.create)
       .delete('/:id', this.delete)
   }
-
-  async create(req, res, next){
+  async create(req, res, next) {
     try {
       req.body.creatorId = req.userInfo.id
       const review = await reviewsService.create(req.body)
@@ -22,7 +21,7 @@ export class ReviewsController extends BaseController{
     }
   }
 
-  async delete(req, res, next){
+  async delete(req, res, next) {
     try {
       const message = await reviewsService.delete(req.params.id, req.userInfo.id)
       return res.send(message)
