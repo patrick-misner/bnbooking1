a549ec27fa9a4792a6fa73ed69bb53662e70cc85
Jeffrey Watson <watsonjeffrey44@gmail.com>
2022-07-14 11:47:03 -0600

    yes sir

--- bnbooking1.client/src/pages/AccountPage.vue
+++ bnbooking1.client/src/pages/AccountPage.vue
@@ -1,38 +1,84 @@
 <template>
-  <div class="about text-center">
-    <h1>Welcome {{ account.name }}</h1>
-    <img class="rounded" :src="account.picture" alt="" />
-    <p>{{ account.email }}</p>
-  </div>
-
-  <div class="container-fluid">
+  <div class="container">
     <div class="row">
-      <div class="col-12">
-        <div class="bg-secondary text-center elevation-2 rounded py-3">
-          <h4>My Client Appointments</h4>
+      <div class="col-3 border border-dark p-3 m-3">
+        <div class="row">
+          <div class="about text-center">
+            <h1>Welcome</h1>
+            <img class="account-picture" :src="account.picture" alt="" />
+            <p>{{ account.name }}</p>
+            <i class="mdi mdi-pen"></i>
+          </div>
         </div>
-
-        <div class="col-12">
-          <ClientAppointment
-            v-for="a in myProviderAppointments"
-            :key="a.id"
-            :appointment="a"
-          />
-        </div>
-
-        <div class="bg-secondary text-center elevation-2 rounded py-3">
-          <h4>My Appointments</h4>
+        <div class="row">
+          <div class="col-12 d-flex justify-content-between">
+            <p><i class="mdi mdi-map-marker"></i>From</p>
+            <span>Boise</span>
+          </div>
+          <div class="col-12"></div>
         </div>
       </div>
-      <div class="col-12">
-        <Appointment
-          v-for="a in userAppointments"
-          :key="a.id"
-          :appointment="a"
-        />
+      <div class="col-9 justify-content-center">
+        <div class="row">
+          <div class="container-fluid">
+            <div class="row">
+              <div class="col-12 m-3">
+                <!-- <i
+          class="mdi mdi-magnify fs-1 text-primary selectable"
+          data-bs-toggle="collapse"
+          data-bs-target="#collapseExample"
+        ></i>
+      </router-link>
+      <div class="collapse" id="collapseExample">
+        <Search />
+      </div> -->
+
+                <div class="bg-secondary text-center elevation-2 rounded py-3">
+                  <h4>
+                    My Client Appointments
+                    <i
+                      title="Expand client appointments"
+                      class="mdi mdi-arrow-expand-all selectable"
+                      data-bs-toggle="collapse"
+                      data-bs-target="#clientappt"
+                    ></i>
+                  </h4>
+                </div>
+                <div class="col-12 pb-3 collapse" id="clientappt">
+                  <ClientAppointment
+                    v-for="a in myProviderAppointments"
+                    :key="a.id"
+                    :appointment="a"
+                  />
+                </div>
+
+                <div class="bg-secondary text-center elevation-2 rounded py-3">
+                  <h4>
+                    My Appointments
+                    <i
+                      title="Expand client appointments"
+                      class="mdi mdi-arrow-expand-all selectable"
+                      data-bs-toggle="collapse"
+                      data-bs-target="#appt"
+                    ></i>
+                  </h4>
+                </div>
+                <div class="col-12 collapse" id="appt">
+                  <Appointment
+                    v-for="a in userAppointments"
+                    :key="a.id"
+                    :appointment="a"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
+
+  <!-- -->
 </template>
 
 <script>
@@ -58,5 +104,8 @@ export default {
 <style scoped>
 img {
   max-width: 100px;
+}
+.account-picture {
+  border-radius: 50%;
 }
 </style>
