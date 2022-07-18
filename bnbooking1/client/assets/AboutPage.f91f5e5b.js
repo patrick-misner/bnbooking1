7b4fb96966d46f4fcc1b5e383d294bb7976dbd24
Patrick Misner <misner.patrick@gmail.com>
2022-07-18 01:33:44 -0600

    npmbuild

---
+++ bnbooking1/client/assets/AboutPage.f91f5e5b.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index.a7d9b559.js";import{o,b as t,d as s}from"./vendor.0d325e26.js";const a={name:"AboutPage"},n={class:"about"},r=s("h1",null,"This is the about page",-1),c=[r];function _(d,i,p,u,f,h){return o(),t("div",n,c)}var b=e(a,[["render",_]]);export{b as default};
